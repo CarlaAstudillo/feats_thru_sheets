c15bce0579c90e2404329e6549586268ed9b78e7
Carla <carla.a.astudillo@gmail.com>
2015-11-24 12:51:03 -0500

    Connected Google Spreadsheet using Tabletop.js

--- js/init.js
+++ js/init.js
@@ -1,116 +1,122 @@
-$(window).load(function(){
+(window).load(function() {
 
-      
-
-
-                   $.each(json, function (i, v) {
-             $('#content').append('<div class="element-item" data-category="transition"><div class="category">'+v.Where+'</div><img src="' + v.Pic_Link + '"><div class="name">'+v.Name+'</div><div class="where">'+ v.Category +'</div><div class="head4">Age: ' + v.Age + '</div><div class="description">' + v.Description + '</div><div class="head4">Nationality: '+ v.Nationality +'</div><div class="readmore">Read <a href="'+ v.Link +' " target="_blank">more</a></div></div>');
-
-
+    var qsRegex;
+    var buttonFilter;
+    var $quicksearch = $('#quicksearch');
+    var $container = $('#content')
+    var timeout;
 
 
-             
-         });
-
-                   
-
-                  
-                  
-var qsRegex;
-var buttonFilter;
-
-  var $quicksearch = $('#quicksearch').keyup( debounce( function() {
-        qsRegex = new RegExp( $quicksearch.val(), 'gi' );
-        $container.isotope();
-      }) );
-                 
-
-       var $container = $('#content')
-       $container.imagesLoaded( function() {        
-
-                $container.isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'masonry',
-    filter: function() {
-      var $this = $(this);
-      var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-      var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-      return searchResult && buttonResult;
-    }
-    
-
-  });
-                 });
+    Tabletop.init({
+        key: '1Jn4I3FIerKG5UGJSq3c3bEDs_V_9OxIGUnJ-Bu-3vEQ',
+        callback: getTable,
+        simpleSheet: true
+    })
 
 
-                
-  
 
-  
+    function getTable(data, tabletop) {
+        $.each(data, function(i, v) {
 
-  var filterFns = {
+            $('#content').append('<div class="element-item" data-category="transition"><div class="category">' + v.where + '</div><img src="' + v.piclink + '"><div class="name">' + v.name + '</div><div class="where">' + v.category + '</div><div class="head4">Age: ' + v.age + '</div><div class="description">' + v.description + '</div><div class="head4">Nationality: ' + v.nationality + '</div><div class="readmore">Read <a href="' + v.link + ' " target="_blank">more</a></div></div>');
 
-    choice1: function() {
-      var name = $(this).find('.category').text();
-      return name.match( /Stade de France$/ );
-    },
-    choice2: function() {
-      var name = $(this).find('.category').text();
-
-      return name.match( /Bataclan concert hall$/ ) || name.match( /Bataclan Concert hall$/ );
-    },
-    choice3: function() {
-      var name = $(this).find('.category').text();
-      return name.match( /Comptoir Voltaire Cafe$/ ) || name.match( /Comptoir Voltaire Cafe$/ );
-    },
-    choice4: function() {
-      var name = $(this).find('.category').text();
-      return name.match( /Remains at large$/ );
-    },
-    choice5: function() {
-      var name = $(this).find('.category').text();
-      return name.match( /Saint-Denis$/ );
-    }
-
-  };
-  // bind filter on select change
-  $('#sorts').on( 'click', 'button', function() {
-    // get filter value from option value
-    buttonFilter = $(this).attr('data-value');
-  
-    // use filterFn if matches value
-    buttonFilter = filterFns[ buttonFilter ] || buttonFilter;
-    $container.isotope();
-  });
+        });
 
 
 
 
-function debounce( fn, threshold ) {
-  var timeout;
-  return function debounced() {
-    if ( timeout ) {
-      clearTimeout( timeout );
-    }
-    function delayed() {
-      fn();
-      timeout = null;
-    }
-    timeout = setTimeout( delayed, threshold || 100 );
-  }
-}
+        $quicksearch.keyup(debounce(function() {
+            qsRegex = new RegExp($quicksearch.val(), 'gi');
+            $container.isotope();
+        }));
 
 
-  $('.btn-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
-    });
-  });
-               
 
-       
- 
-  
+        $container.imagesLoaded(function() {
+
+            $container.isotope({
+                itemSelector: '.element-item',
+                layoutMode: 'masonry',
+                filter: function() {
+                    var $this = $(this);
+                    var searchResult = qsRegex ? $this.text().match(qsRegex) : true;
+                    var buttonResult = buttonFilter ? $this.is(buttonFilter) : true;
+                    return searchResult && buttonResult;
+                }
+
+
+            });
+        });
+
+
+
+
+        var filterFns = {
+
+            choice1: function() {
+                var name = $(this).find('.category').text();
+                return name.match(/Stade de France$/);
+            },
+            choice2: function() {
+                var name = $(this).find('.category').text();
+
+                return name.match(/Bataclan concert hall$/) || name.match(/Bataclan Concert hall$/);
+            },
+            choice3: function() {
+                var name = $(this).find('.category').text();
+                return name.match(/Comptoir Voltaire Cafe$/) || name.match(/Comptoir Voltaire Cafe$/);
+            },
+            choice4: function() {
+                var name = $(this).find('.category').text();
+                return name.match(/Remains at large$/);
+            },
+            choice5: function() {
+                var name = $(this).find('.category').text();
+                return name.match(/Saint-Denis$/);
+            }
+
+        };
+        // bind filter on select change
+        $('#filter').on('click', 'button', function() {
+            // get filter value from option value
+            buttonFilter = $(this).attr('data-value');
+
+            // use filterFn if matches value
+            buttonFilter = filterFns[buttonFilter] || buttonFilter;
+            $container.isotope();
+        });
+
+
+
+
+        function debounce(fn, threshold) {
+
+            return function debounced() {
+                if (timeout) {
+                    clearTimeout(timeout);
+                }
+
+                function delayed() {
+                    fn();
+                    timeout = null;
+                }
+                timeout = setTimeout(delayed, threshold || 100);
+            }
+        }
+
+
+        $('.btn-group').each(function(i, buttonGroup) {
+            var $buttonGroup = $(buttonGroup);
+            $buttonGroup.on('click', 'button', function() {
+                $buttonGroup.find('.is-checked').removeClass('is-checked');
+                $(this).addClass('is-checked');
+            });
+        });
+
+
+    };
+
+
+
+
 });
