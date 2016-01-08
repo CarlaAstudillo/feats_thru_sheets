e94701cd0d085a4aefa33e575c88df379b85d64b
CarlaAstudillo <carla.a.astudillo@gmail.com>
2016-01-08 17:10:54 -0500

    Card flippy for SOTU

---
+++ word_database/js/init.js
@@ -0,0 +1,209 @@
+$(window).load(function() {
+
+    $("div#card").flip();
+
+    var qsRegex;
+    var buttonFilter;
+    var $quicksearch = $('#quicksearch');
+    var $container = $('#database')
+    var timeout;
+   
+    var public_spreadsheet_url = '1Yg7etMyeGq3Q5D2LUhuvjtoNBTPWdpaWrVU5n90erlQ';
+
+    var timestampdata = "https://spreadsheets.google.com/feeds/cells/" + public_spreadsheet_url + "/od6/public/basic?alt=json"
+
+    // Call the Google Spreadsheet as a regular JSON to get latest timestamp which is not included in Tabletop.js
+
+
+    $.ajax({
+        url: timestampdata,
+        dataType: "jsonp",
+        success: function(data) {
+            // Get timestamp and parse it to readable format
+
+
+            var date = data.feed.updated.$t
+
+            var MM = {
+                Jan: "Jan.",
+                Feb: "Feb.",
+                Mar: "March",
+                Apr: "April",
+                May: "May",
+                Jun: "June",
+                Jul: "July",
+                Aug: "Aug.",
+                Sep: "Sept.",
+                Oct: "Oct.",
+                Nov: "Nov.",
+                Dec: "Dec."
+            }
+
+            var formatdate = String(new Date(date)).replace(
+                /\w{3} (\w{3}) (\d{2}) (\d{4}) (\d{2}):(\d{2}):[^(]+\(([A-Z]{3})\)/,
+                function($0, $1, $2, $3, $4, $5, $6) {
+                    return MM[$1] + " " + $2 + ", " + $3 + " at " + $4 % 12 + ":" + $5 + (+$4 > 12 ? "PM" : "AM") + " " + $6
+                }
+            )
+
+
+            $('.updated').append("Last updated " + formatdate)
+        },
+    });
+
+
+
+    // Tabletop initialization
+
+    Tabletop.init({
+        key: public_spreadsheet_url,
+        callback: getTable,
+        simpleSheet: true
+    })
+
+    // Function that fetches the Google Spreadsheet
+
+
+    function getTable(data, tabletop) {
+
+        var sheetname = tabletop.foundSheetNames[0];
+        var sheetnamecontrol = tabletop.foundSheetNames[1];
+
+        // Get title of datasheet
+
+        var title = sheetname;
+        $("h2").append(title)
+
+        // Get credits and explainer from "Control spreadsheet"
+
+        $.each(tabletop.sheets(sheetnamecontrol).all(), function(i, v) {
+
+            var explainer = v.explainer
+            var credits = v.credits
+            $(".credit").append(credits)
+            $(".explainer").append(explainer)
+        });
+
+        var result = [];
+        var count = 1;
+
+
+        $.each(tabletop.sheets(sheetname).all(), function(i, v) {
+
+
+            // Parses the resulting JSON into the individual squares for each row
+
+            $container.append('<div id="card"><div class="front"><div class="name">' + v.title + '</div></div><div class="back"><div class="colorsubhed"><span>"</span>' + v.subhed1 + '<span>"</span></div><div class="boldsubhed">--<a href="' + v.link + ' " target="_blank">' + v.subhed2 + '</a></div><div class="category">' + v.filtercategory + '</div></div></div>');
+
+
+            // Gets all unique filtercategory values and puts them into an array
+            if ($.inArray(v.filtercategory, result) == -1) {
+
+                result.push(v.filtercategory);
+
+                // Creates the filter buttons
+
+                $('#filter').append('<button id="' + v.filtercategory + '" class="btn btn-default" data-value="choice' + count++ + '">' + v.filtercategory + '</button>')
+
+            }
+
+
+
+        });
+
+        // Adds the search function
+
+
+        $quicksearch.keyup(debounce(function() {
+            qsRegex = new RegExp($quicksearch.val(), 'gi');
+            $container.isotope();
+        }));
+
+
+        // Sorts them into responsive square layout using isotope.js
+        $container.imagesLoaded(function() {
+
+            $container.isotope({
+                itemSelector: '#card',
+                layoutMode: 'masonry',
+                // so that isotope will filter both search and filter results
+                filter: function() {
+                    var $this = $(this);
+                    var searchResult = qsRegex ? $this.text().match(qsRegex) : true;
+
+                    var buttonResult = buttonFilter ? $this.is(buttonFilter) : true;
+
+                    return searchResult && buttonResult;
+
+                }
+
+            });
+
+
+        });
+
+
+
+        // debounce so filtering doesn't happen every millisecond
+        function debounce(fn, threshold) {
+
+            return function debounced() {
+                if (timeout) {
+                    clearTimeout(timeout);
+                }
+
+                function delayed() {
+                    fn();
+                    timeout = null;
+                }
+                timeout = setTimeout(delayed, threshold || 100);
+            }
+        }
+
+       
+
+        // Adds a click function to all buttons in the group
+
+        $('.btn-group').each(function(i, buttonGroup) {
+            var $buttonGroup = $(buttonGroup);
+            var allbuttonids = $("button").attr('id');
+            $buttonGroup.on('click', 'button', function() {
+
+                // Changes to .is-checked class when clicked
+
+                $buttonGroup.find('.is-checked').removeClass('is-checked');
+                $(this).addClass('is-checked');
+
+                // Gets all values that matches the clicked button's data value
+
+                buttonFilter = $(this).attr('data-value');
+                textFilter = $(this).text();
+
+
+                function getitems() {
+                    var name = $(this).find('.category').text();
+
+                    if (textFilter != "Show All") {
+                        return name.match(textFilter);
+
+                    } else {
+                        return "*";
+                    }
+
+                }
+
+                buttonFilter = getitems || buttonFilter;
+
+                $container.isotope();
+
+
+            });
+        });
+
+ 
+
+
+    };
+
+
+});
